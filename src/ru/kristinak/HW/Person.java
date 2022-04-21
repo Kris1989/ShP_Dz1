6f77ac1efe3a2b4c3647a31f2208114d6631b1e0
Kris1989 <kki1989@mail.ru>
2022-04-21 10:56:35 +0300

    Commit

---
+++ src/ru/kristinak/HW/Person.java
@@ -0,0 +1,86 @@
+package ru.kristinak.HW;
+
+import java.util.Objects;
+import java.util.OptionalInt;
+
+public class Person {
+    protected final String name;
+    protected final String surname;
+    protected OptionalInt age;
+    protected String address;
+
+    public Person(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public Person(String name, String surname, int age) {
+        this.name = name;
+        this.surname = surname;
+        this.age = OptionalInt.of(age);
+    }
+
+    public Person(String name, String surname, int age, String address) {
+        this.name = name;
+        this.surname = surname;
+        this.age = OptionalInt.of(age);
+        this.address = address;
+    }
+
+    public PersonBuilder newChildBuilder() {
+        return new PersonBuilder()
+                .setSurname(this.surname)
+                .setAge(0)
+                .setAddress(this.address);
+    }
+
+    public boolean hasAge() {
+        return age.isPresent();
+    }
+
+    public boolean hasAddress() {
+        return address != null;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public OptionalInt getAge() {
+        return age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String city) {
+        address = city;
+    }
+
+    public void happyBirthday() {
+        if (this.age.isEmpty()) {
+            throw new IllegalArgumentException("Заполните поле возраста");
+        }
+        this.age = OptionalInt.of(1 + age.getAsInt());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, surname, age, address);
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", age=" + age.getAsInt() +
+                ", address='" + address + '\'' +
+                '}';
+    }
+}
